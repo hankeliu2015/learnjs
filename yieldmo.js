779d4fbc1fff79aa72c3f593d84b7026969c7a63
github username <github email address>
2019-12-14 23:27:12 -0500

    add new paragraph

--- yieldmo.js
+++ yieldmo.js
@@ -45,24 +45,3 @@ yieldmo();
 //output button 0, button 1, button 2, button 3, button4
 // the result is no matter which button you click, it will return 5 in console  log.
 // need to know why and how to change it to the correct output.
-
-// setting button text via js - solution2:
-
-var b = document.createElement('button');
-b.setAttribute('content', 'test content');
-b.setAttribute('class', 'btn');
-// b.innerHTML = 'Button - Created by JS';
-b.appendChild(document.createTextNode('Button - Created by JS'));
-
-var wrapper = document.getElementById("divWrapper");
-wrapper.appendChild(b);
-
-//settng a button value using js:
-function myFunc(form) {
-    form.elements["submit-button"].innerHTML = "New<br>Text";
-    return false;
-}
-
-// js manipulate DOM test:
-var container = document.getElementById("container");
-  container.innerHTML = "Empty container added New Content!";
